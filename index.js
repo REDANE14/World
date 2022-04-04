9374bd2a43b31177bbcece2facdd66d36390be66
GARRED ALDANE B. PAGUIO <101236149+REDANE14@users.noreply.github.com>
2022-04-04 11:35:33 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-button = document.getElementById("btn");
-
-
-button.addEventListener('click', function() {
-    changeText = document.getElementById('text');
-    changeText.textContent = "I love you!";
-    document.getElementById('btn').disabled = true;
-});
